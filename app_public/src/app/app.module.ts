5e7ab4ea3b4c133b7e869ac63b193807596985f4
Marcelo Martins <m.a.martins@live.com>
2019-06-07 14:38:52 -0300

    Building a single-page application with Angular: Foundations

---
+++ app_public/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
+import { RatingStarsComponent } from './rating-stars/rating-stars.component';
+
+@NgModule({
+  declarations: [
+    HomeListComponent,
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    SidebarComponent,
+    HtmlLineBreaksPipe,
+    RatingStarsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomepageComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [FrameworkComponent]
+})
+export class AppModule {
+}
---
+++ app_public/src/app/home-list/home-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Loc8rDataService } from "../loc8r-data.service";
+import { GeolocationService } from "../geolocation.service";
+
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
+
+@Component({
+  selector: 'app-home-list',
+  templateUrl: './home-list.component.html',
+  styleUrls: ['./home-list.component.css']
+})
+export class HomeListComponent implements OnInit {
+  public locations:Location[];
+  public message: string;
+
+  constructor(private  dataService: Loc8rDataService,
+              private geolocationService: GeolocationService) {
+  }
+
+  ngOnInit() {
+    this.getPosition();
+  }
+
+  private getLocations(position: any): void {
+    this.message = 'Searching for nearby places';
+    const lat: number = position.coords.latitude;
+    const lng: number = position.coords.longitude;
+    this.dataService.getLocations(lat, lng)
+      .then(foundLocations => {
+        this.message = foundLocations.length > 0 ? '' : 'No locations found';
+        this.locations = foundLocations;
+      });
+  }
+
+  private getPosition(): void {
+    this.message = 'Getting your position...';
+    this.geolocationService.getPosition(
+      this.getLocations.bind(this),
+      this.showError.bind(this),
+      this.noGeo.bind(this)
+    );
+  }
+
+  private showError(error: any): void {
+    this.message = error.message;
+  }
+
+  private noGeo(): void {
+    this.message = 'Geolocation not supported by this browser.';
+  }
+}
